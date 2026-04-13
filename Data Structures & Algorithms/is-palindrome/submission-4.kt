ac90fb1dc9c4586b8824114fe54e983823142abe
Sankamita21 <68897383+Sankamita21@users.noreply.github.com>
2026-04-13 14:06:51 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+
+        val input = s.lowercase().filter{it.isLetterOrDigit()}
+
+        var left =0
+        var right = input.length-1
+
+        while(left<right){
+            if(input[left] != input[right]){
+
+                return false
+              
+            }
+              left++
+            right--
+           
+        }
+        return true
+    }
+}
